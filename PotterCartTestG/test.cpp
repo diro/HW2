fddf9f1686f13b6f2bb5ef0483ee892077b2f0d9
Diro <diro.fan@gmail.com>
2018-02-13 02:00:52 +0800

    rename test case

--- PotterCartTestG/test.cpp
+++ PotterCartTestG/test.cpp
@@ -10,9 +10,9 @@ TEST(PotterCart, buy_1_vol1_total_should_be_100dollars)
 	sut.AddToCart(items);
 
 	int actual = sut.GetTotal();
-	
+
 	int expected = 100;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
 TEST(PotterCart,buy_1_vol2_total_should_be_100dollars)
@@ -21,11 +21,11 @@ TEST(PotterCart,buy_1_vol2_total_should_be_100dollars)
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('2', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 100;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
 TEST(PotterCart, buy_1_vol3_total_should_be_100dollars)
@@ -36,9 +36,9 @@ TEST(PotterCart, buy_1_vol3_total_should_be_100dollars)
 	sut.AddToCart(items);
 
 	int actual = sut.GetTotal();
-	
+
 	int expected = 100;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
 TEST(PotterCart, buy_1_vol4_total_should_be_100dollars)
@@ -47,11 +47,11 @@ TEST(PotterCart, buy_1_vol4_total_should_be_100dollars)
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('4', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 100;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
 TEST(PotterCart, buy_1_vol5_total_should_be_100dollars)
@@ -60,28 +60,28 @@ TEST(PotterCart, buy_1_vol5_total_should_be_100dollars)
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('5', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 100;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_2_different_total_should_be_190dollars)
+TEST(PotterCart, buy_1_1_should_be_190dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('1', 1));
 	items.episodeCountList.insert(std::make_pair('2', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 190;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_3_different_total_should_be_270dollars)
+TEST(PotterCart, buy_1_1_1_should_be_270dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
@@ -89,14 +89,14 @@ TEST(PotterCart, buy_3_different_total_should_be_270dollars)
 	items.episodeCountList.insert(std::make_pair('2', 1));
 	items.episodeCountList.insert(std::make_pair('3', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 270;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_4_different_total_should_be_320dollars)
+TEST(PotterCart, buy_1_1_1_1_should_be_320dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
@@ -105,14 +105,14 @@ TEST(PotterCart, buy_4_different_total_should_be_320dollars)
 	items.episodeCountList.insert(std::make_pair('3', 1));
 	items.episodeCountList.insert(std::make_pair('5', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 320;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_5_different_total_should_be_375dollars)
+TEST(PotterCart, buy_1_1_1_1_1_should_be_375dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
@@ -122,56 +122,56 @@ TEST(PotterCart, buy_5_different_total_should_be_375dollars)
 	items.episodeCountList.insert(std::make_pair('4', 1));
 	items.episodeCountList.insert(std::make_pair('5', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 375;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_2vol1_and1_vol2_total_should_be_290dollars)
+TEST(PotterCart, buy_2_1_should_be_290dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('1', 2));
 	items.episodeCountList.insert(std::make_pair('2', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 290;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_2vol1_and2_vol2_total_should_be_380dollars)
+TEST(PotterCart, buy_2_2_should_be_380dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('1', 2));
 	items.episodeCountList.insert(std::make_pair('2', 2));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 380;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_3vol1_and1_vol2_total_should_be_390dollars)
+TEST(PotterCart, buy_3_1_should_be_390dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
 	items.episodeCountList.insert(std::make_pair('1', 3));
 	items.episodeCountList.insert(std::make_pair('2', 1));
 	sut.AddToCart(items);
-	
+
 	int actual = sut.GetTotal();
-	
+
 	int expected = 390;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart,buy_8_different_total_should_be_640dollars)
+TEST(PotterCart,buy_2_2_2_1_1_should_be_640dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
@@ -185,10 +185,10 @@ TEST(PotterCart,buy_8_different_total_should_be_640dollars)
 	int actual = sut.GetTotal();
 
 	int expected = 640;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
-TEST(PotterCart, buy_9_different_total_should_be_695dollars)
+TEST(PotterCart, buy_2_2_2_1_1_should_be_695dollars)
 {
 	PotterCart sut;
 	ShoppingItem items;
@@ -202,7 +202,7 @@ TEST(PotterCart, buy_9_different_total_should_be_695dollars)
 	int actual = sut.GetTotal();
 
 	int expected = 695;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
 
 
@@ -220,5 +220,5 @@ TEST(PotterCart, buy_4_4_4_2_2_should_be_695dollars)
 	int actual = sut.GetTotal();
 
 	int expected = 1280;
-	ASSERT_EQ(expected, actual);
+ASSERT_EQ(expected, actual);
 }
